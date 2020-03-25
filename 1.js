1862ce70fde6c561009c41dca180b5902df2c9d7
ANDER AAM <arevalomadrid62776@gmail.com>
2020-03-25 00:54:52 -0500

    version noche 2.0

--- 1.js
+++ 1.js
@@ -1,50 +1,42 @@
+
+
+function GenerarT(){
+  var x = 8 ;
+  var tabla="<table border=\"0\">";
+  for(i=0;i<8;i++){
+  tabla+="<tr>";
+  for(j=0;j<8;j++){
+  tabla+="<td>"+"<IMG src=\"Bloque.ico\">"+ "</td>";
+  }
+  tabla+="</tr>";
+  }
+  tabla+="</table>";
+  document.getElementById("Resultado").innerHTML=tabla;
+  alert("Usted Seleccionado el Nivel Facil : ");
+};
+
+
+
 function CrearTabla(){
-/*
-facil   8 x 8
-medio   16 x 16
-dificil 32 x 32
-*/
 
 var Nombre = document.getElementById("Nombre").value;
 var Op1    = document.getElementById("Facil");
 var Op2    = document.getElementById("Medio");
 var Op3    = document.getElementById("Dificil");
 
+
  if (Op1.checked==true)
-  alert("Usted Seleccionado el Nivel Facil : "+Op1.value);
+  GenerarT();
  else if (Op2.checked==true)
    alert("Usted Seleccionado el nivel Medio  "+Op2.value);
  else if (Op3.checked==true)
    alert("Usted Seleccionado el nivel Dificil : "+Op3.value);
 
-   GenerarT()
-}
-
-function GenerarT(){
-
-    for(i=0;i<8;i++){
-    for(j=0;j<8;j++){
-    var div = document.createElement("div");
-    div.id =i+""+j;
-    }
-    }
-
-
-    document.getElementById("Resultado").innerHTML=div;
-    alert("SI ESTA!!!")
-}
-
-
-
-
-
-
-
-
+};
 
 
 /*for(i=0;i<7;i++){
-for(j=0;j<4;j++){
+for(j=0;j<4s;j++){
 var div = document.createElement("div");
 div.id =i+""+j;
 }
